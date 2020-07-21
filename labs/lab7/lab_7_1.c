e49455b5a3efe401f0e731968e45240ff1920c0e
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-07-21 22:45:43 +1200

    better version of lab 7

--- labs/lab7/lab_7_1.c
+++ labs/lab7/lab_7_1.c
@@ -14,23 +14,40 @@ void *emalloc(size_t n){
     return result;
 }
 
+void print_above_average(char **word_list, int length, double average){
+    if (length > 0) {
+        if (strlen(word_list[0]) > average) {
+            printf("%s\n",word_list[0]);
+        }
+        print_above_average(word_list + 1, length - 1, average); 
+    }
+}
+
 int main(void) {
     char word[ARRAY_LEN];
     char *word_list[NUM_WORDS];
     int num_words=0;
-    double average;
+    double average=0, total=0;
     int i;
 
     while (num_words < NUM_WORDS && 1 == scanf("%79s",word)){
         word_list[num_words] = emalloc( strlen(word+1) * sizeof word_list[0][0]);
         strcpy(word_list[num_words],word);
+        total += strlen(word);
         num_words++;  
     }
+    
+    average = (total/num_words);
+
+    printf("the average word length is %lg\n",average);
+
+    print_above_average(word_list, num_words, average);
+
+    printf("freeing memory\n");
 
     for(i=0;i<num_words;i++){
-        printf("word %d is %s\n",i,word_list[i]);
+        free(word_list[i]);
     }
 
     return EXIT_SUCCESS;
-    
 }
