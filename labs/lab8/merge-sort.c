59fb0200a8e48cce3bf12ee1151910ebc5025991
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-07-26 14:48:37 +1200

    merge sort working

---
+++ labs/lab8/merge-sort.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ARRAY_MAX 10
+
+/* n is the size of the array */
+void print_array(int *a, int n){
+    if (n > 0 ) {
+        printf("%d\n", a[0]);
+        print_array(a + 1, n - 1);
+    }
+}
+
+void merge(int *array, int *workspace, int len){
+    /*point to beginning of left half*/
+    int i = 0;
+    /*poinr to beginning of right half*/
+    int j = len/2;
+    /*index indicating how many items have been sorted into result array*/
+    int k = 0;
+
+    /*while there are items remaining in both halves*/
+    while((i < len/2) && (j<len)){
+       if (array[i] < array[j]) {
+           workspace[k] = array[i];
+           i++;
+           k++;
+       } else {
+           workspace[k] = array[j];
+           j++;
+           k++;
+       } 
+    }
+   
+    /*first and second halves of array may not finish allocating to
+     * workspace at the same time, so now take care of leftover elements
+     * in the half arrays */
+
+    /*copy over remaining elements from left array to workspace*/
+    while(i < len/2){
+        workspace[k] = array[i];
+        i++;
+        k++;
+    }
+    /*copy over remaining elements from right array to workspace*/
+    while(j < len){
+        workspace[k] = array[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int *array, int *workspace, int len){
+    int i;
+
+    if (len < 2) {
+        return;
+    }
+    
+    /*call merge sort on first half of array*/
+    merge_sort(array,workspace,len/2);
+    /*call merge sort on second half of array*/
+    merge_sort(array + (len/2),workspace, len - (len/2));
+
+    merge(array,workspace,len);
+
+    /*now copy merged, sorted array into place*/
+    for(i=0;i<len;i++){
+        array[i] = workspace[i];
+    }
+}
+
+
+int main(void) {
+    int my_array[ARRAY_MAX];
+    int workspace[ARRAY_MAX];
+    int count = 0;
+
+    while (count < ARRAY_MAX && 1 == scanf("%d", &my_array[count])) {
+        count++;
+    }
+
+    merge_sort(my_array, workspace, count);
+
+    printf("printing array now! \n");
+
+    print_array(&my_array[0],count);
+
+    return EXIT_SUCCESS;
+}
