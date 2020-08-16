2df55b18788fab9ef32c6d896fb56cbfa6a65c5f
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-08-16 19:38:12 +1200

    A working

--- labs/lab13/A.c
+++ labs/lab13/A.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isprime(int n) {
+int is_prime (int num) {
     int i;
-    for (i=2;i<n;i++) {
-        if (n%i==0) {
-            return 0; /*number is not prime*/
+    for (i=2;i<num;i++) {
+        if (num%i==0) {
+            return 0;
         }
     }
-    return 1;   /*number is prime...*/
+    return 1;
 }
 
 int main(void) {
-    int i=2; /*print first 200 prime numbers*/
-    int num_printed=0;
-    while (num_printed < 200) {
-        if(isprime(i)) {
-            printf("%5d",i);
-            num_printed++;
-            if (num_printed%10==0) {
+    int num = 2;
+    int nums_printed = 0;
+    while (nums_printed < 200) {
+        if (is_prime(num)) {
+            printf("%5d",num);
+            nums_printed++;
+            if (nums_printed%10==0) {
                 printf("\n");
             }
         }
-        i++;
+        num++;
     }
     return EXIT_SUCCESS;
 }
