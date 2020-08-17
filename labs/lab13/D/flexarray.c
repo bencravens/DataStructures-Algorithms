492b637381b900e55f4b194fae7d0bf260db88be
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-08-17 18:14:52 +1200

    practiced D

--- labs/lab13/D/flexarray.c
+++ labs/lab13/D/flexarray.c
@@ -9,26 +9,22 @@ struct flexarrayrec {
 };
 
 void insertion_sort(int* a, int len) {
-    int i;
-    int j;
+    int i,j;
     int key;
-
     for (i=1;i<len;i++) {
-        key = a[i];
         j = i - 1;
-        while (key < a[j] && j>=0 ) {
-            /*shift all elements to the left of key up (as long as they are less than key)*/
+        key = a[i];
+        while (j>=0 && key<a[j]) {
             a[j+1] = a[j];
-            j--; 
-        }   
-        /*now put key in place*/
+            j = j - 1;
+        }
         a[j+1] = key;
-    }   
+    }
 }
 
 void* emalloc(size_t n) {
     void* result = malloc(n);
-    if (result == NULL) {
+    if (result==NULL) {
         fprintf(stderr,"memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
@@ -38,38 +34,35 @@ void* emalloc(size_t n) {
 void* erealloc(int* a, size_t n) {
     void* result = realloc(a,n);
     if (result==NULL) {
-        fprintf(stderr,"memory reallocation failed.\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr,"memory reallocation failed!\n");
     }
     return result;
 }
 
 flexarray flexarray_new() {
     flexarray result = emalloc(sizeof *result);
-    result->capacity=2;
-    result->itemcount=0;
+    result->capacity = 2;
+    result->itemcount = 0;
     result->items = emalloc(result->capacity * sizeof result->items[0]);
     return result;
 }
 
 void flexarray_append(flexarray f, int num) {
-    /*reached capacity, reallocate*/
     if (f->itemcount==f->capacity) {
-        f->capacity=2*(f->capacity);
-        f->items = erealloc(f->items, f->capacity * sizeof f->items[0]);
+        f->capacity=2*f->capacity;
+        f->items=erealloc(f->items,f->capacity * sizeof f->items[0]);
     }
-    /*add new item*/
     f->items[f->itemcount] = num;
-    f->itemcount++; 
+    f->itemcount++;
 }
 
 void flexarray_sort(flexarray f) {
-    insertion_sort(f->items, f->itemcount);
+    insertion_sort(f->items,f->itemcount);
 }
 
 void flexarray_print(flexarray f) {
     int i;
-    for(i=0;i<f->itemcount;i++) {
+    for (i=0;i<f->itemcount;i++) {
         printf("%d\n",f->items[i]);
     }
 }
