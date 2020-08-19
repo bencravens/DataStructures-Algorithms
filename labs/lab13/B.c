4258029051b3a345d4f7962f2978f917ad04c8f5
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-08-19 12:26:25 +1200

    practiced B

--- labs/lab13/B.c
+++ labs/lab13/B.c
@@ -9,31 +9,23 @@ double smaller(double a, double b) {
     }
 }
 
-double bigger(double a, double b) {
-    if (a > b) { 
-        return a;
-    } else {
-        return b;
-    } 
-}
-
 int main(void) {
-    double s1, s2, s3;
+    double s1,s2,s3;
     int id;
-    int winner_id;
     double min;
     double score;
+    int winner_id;
     double winner_score=0;
-    
+        
     while(4==scanf("%d%lg%lg%lg",&id,&s1,&s2,&s3)) {
         min = smaller(s1,smaller(s2,s3));
         if (min==s1) {
-            score = (s2 + s3)/2;
+            score = (s2 + s3) / 2;
         } else if (min==s2) {
-            score = (s1 + s3)/2;
-        } else { 
-            score = (s1 + s2)/2;
-        } 
+            score = (s1 + s3) / 2;
+        } else {
+            score = (s1 + s2) / 2;
+        }   
         if (score > winner_score) {
             winner_score = score;
             winner_id = id;
@@ -41,6 +33,6 @@ int main(void) {
     }
 
     printf("%d\n",winner_id);
-    
+
     return EXIT_SUCCESS;
 }
