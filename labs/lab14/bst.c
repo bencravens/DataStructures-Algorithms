155f8feac5d5bb5196498cc80c74d3400c753f63
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-08-19 18:44:34 +1200

    finished binary search tree (delete method not working yet)

---
+++ labs/lab14/bst.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bst.h"
+
+struct bst_node {
+    char* key;
+    bst left;
+    bst right;
+};
+
+void print_key(char* s) {
+    printf("%s\n", s);
+}
+
+void* emalloc(size_t n) {
+    void* result = malloc(n);
+    if (result == NULL) {
+        fprintf(stderr, "memory allocation failed\n");
+    }
+    return result;
+}
+
+bst bst_new() {
+    return NULL;
+}
+
+void bst_inorder(bst b, void f(char* str)) {
+    if (b==NULL) {
+        return;
+    }
+    bst_inorder(b->left,f);
+    f(b->key);
+    bst_inorder(b->right,f);
+}
+
+void bst_preorder(bst b, void f(char* str)) {
+    if (b==NULL) {
+        return;
+    }
+    f(b->key);
+    bst_preorder(b->left,f);
+    bst_preorder(b->right,f);
+}
+
+bst bst_insert(bst b, char* str) {
+    if (b==NULL) {
+        b = emalloc(sizeof *b);
+        b->key = emalloc((strlen(str)+1) * sizeof str[0]);
+        b->key=str;
+    } else if (strcmp(b->key,str)==0) {
+        ;
+    } else if (strcmp(str,b->key)<0) {
+        b->left = bst_insert(b->left, str);
+    } else {
+        b->right = bst_insert(b->right,str);
+    }
+    return b;
+}
+
---
+++ labs/lab14/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bst.h"
+#include "mylib.h"
+
+#define ARRAY_LEN 10
+#define NUM_WORDS 10
+
+int main(void) {
+    bst b = bst_new();
+    char word[ARRAY_LEN];
+    char* word_list[NUM_WORDS];
+    int num_words = 0;
+    int i;
+    /*read in a bunch of words*/
+    while ((num_words < NUM_WORDS) && (1==scanf("%79s",word))) {
+        word_list[num_words] = malloc((strlen(word)+1) * sizeof word_list[0][0]);
+        strcpy(word_list[num_words],word);
+        num_words++;
+    }
+    /* insert words into BST */
+    for (i=0;i<num_words;i++) {
+        b = bst_insert(b,word_list[i]);
+    } 
+    printf("printing all now\n");
+    bst_inorder(b,print_key);
+    return EXIT_SUCCESS;
+}
