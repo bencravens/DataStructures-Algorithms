6ba771b460710594ec50db2e762c615106540cb1
bcravens <bcravens@hex.default.uod.otago.ac.nz>
2020-09-24 15:36:13 +1200

    finished f (depth first search)

---
+++ practical_3/lab22f/graph.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "graph.h"
+#include "mylib.h"
+#include "queue.h"
+
+typedef enum state {UNVISITED, VISITED_SELF, VISITED_DESCENDANTS} state_t;
+
+struct graphrec {
+    int* pred;
+    int* distance;
+    state_t* state;
+    int* finish;
+    int** edges;
+    int size;
+};
+
+static int step;
+
+graph graph_new(int num_vertices) {
+    int i;
+    int j;
+    graph g = emalloc(sizeof *g);
+    g->size = num_vertices; 
+    g->pred = emalloc(num_vertices * sizeof g->pred[0]);
+    g->distance = emalloc(num_vertices * sizeof g->distance[0]);
+    g->state = emalloc(num_vertices * sizeof g->state[0]);
+    g->finish = emalloc(num_vertices * sizeof g->finish[0]);
+    /*as the edge matrix is represented by a array of rows ( also arrays ) we
+     * have to allocate memory to these iteratively*/
+    g->edges = emalloc(num_vertices * sizeof g->edges[0]);
+    for (i=0; i<num_vertices; i++) {
+        g->edges[i] = emalloc(num_vertices * sizeof g->edges[0][0]);
+    }
+    /*now initialize edge matrix*/
+    for (i=0; i<num_vertices; i++) {
+        for (j=0; j<num_vertices; j++) {
+            g->edges[i][j] = 0;
+        }
+    }
+    return g;
+}
+
+graph graph_free(graph g) {
+    int i;
+    /*free edges matrix*/
+    for (i=0;i < g->size;i++) {
+        free(g->edges[i]);
+    }
+    free(g->edges);
+    /*free vertex arrays*/
+    free(g->finish);
+    free(g->state);
+    free(g->distance);
+    free(g->pred);
+    /*free graph itself*/
+    free(g);
+    return(g);
+}
+
+void graph_add_2edges(graph g, int u, int v) {
+    g->edges[u][v] = 1;
+    g->edges[v][u] = 1;
+}
+
+void graph_add_edge(graph g, int u, int v) {
+    g->edges[u][v] = 1;
+}
+
+void graph_print_list(graph g) {
+    int i;
+    int j;
+    int count=0;
+    printf("adjacency list:\n");
+    for (i=0;i < g->size;i++) {
+        /*reset count*/
+        count = 0;
+        printf("%d | ",i);
+        for (j=0;j < g->size;j++) {
+            if (g->edges[i][j] == 1) {
+                if (count == 0) {
+                    printf("%d",j);
+                } else {
+                    printf(", %d",j);
+                } 
+                count++;
+            }
+        }
+        printf("\n");
+    }
+}
+
+void visit(graph g, int v) {
+    int u;
+    g->state[v] = VISITED_SELF;
+    step++;
+    g->distance[v] = step;
+    for (u=0; u < g->size; u++) {
+        /*for each unvisited vertex u adjacent to v*/
+        if (g->edges[v][u] == 1 && g->state[u] == UNVISITED) {
+            g->pred[u] = v;
+            visit(g, u);
+        }
+    }
+    step++;
+    g->state[v] = VISITED_DESCENDANTS;
+    g->finish[v] = step;
+}
+
+void graph_dfs(graph g) {
+    int v;
+    for (v=0; v<g->size; v++) {
+        g->state[v] = UNVISITED;
+        g->pred[v] = -1;
+    }
+    
+    step = 0;
+    for (v=0; v<g->size; v++) {
+        if (g->state[v] == UNVISITED) {
+            visit(g,v);
+        }
+    }
+}
+
+
+void graph_print_state(graph g) {
+    int i;
+    printf("vertex distance pred finish\n");
+    for (i=0; i<g->size; i++) {
+        printf("%5d%7d%6d%6d\n",i,g->distance[i],g->pred[i],g->finish[i]);
+    }
+}
---
+++ practical_3/lab22f/graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <stdio.h>
+
+struct graphrec;
+typedef struct graphrec *graph;
+
+extern graph    graph_new(int num_vertices);
+extern graph    graph_free(graph g);
+extern void     graph_add_2edges(graph g, int u, int v);
+extern void     graph_add_edge(graph g, int u, int v);
+extern void     graph_print_list(graph g);
+extern void     graph_print_state(graph g);
+extern void     graph_dfs(graph g); 
+extern void     visit(graph g, int v);
+
+#endif
