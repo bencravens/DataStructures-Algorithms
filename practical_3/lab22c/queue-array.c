645dab90f551cff2aaea47b02f4b11c7735bbef2
bcravens <bcravens@hextreme.default.uod.otago.ac.nz>
2020-09-25 15:11:40 +1200

    circular array queue working.

--- practical_3/lab22c/queue-array.c
+++ practical_3/lab22c/queue-array.c
@@ -4,65 +4,60 @@
 #include "queue.h"
 
 struct queue {
-    int num_items;
-    int capacity;
-    int head;
     double* items;
+    int head;
+    int capacity;
+    int num_items;
 };
 
 queue queue_new() {
-    int default_size = 7;
     int i;
+    int default_size = 7;
     queue q = emalloc(sizeof *q);
-    q->num_items = 0;
-    q->capacity = default_size;
     q->head = 0;
+    q->capacity = default_size;
+    q->num_items = 0;
     q->items = emalloc(q->capacity * sizeof q->items[0]);
-    /*initialize array to 0.0*/
-    for (i=0;i<default_size;i++) {
+    for (i=0;i<q->capacity;i++) {
         q->items[i] = 0.0;
     }
     return q;
 }
 
 void enqueue(queue q, double item) {
-    if (q->num_items < q->capacity) {
+    if (q->num_items == q->capacity) {
+        return;
+    } else {
         q->items[(q->head + q->num_items) % q->capacity] = item;
         q->num_items++;
-    } else if (q->num_items == q->capacity) {
-        /*trying to insert into empty queue, do nothing instead...*/
-        return;
     }
 }
 
 double dequeue(queue q) {
     double item;
     if (q->num_items == 0) {
-        /*cannot dequeue empty queue, do nothing instead...*/
-        exit(EXIT_FAILURE);    
+        fprintf(stderr, "Cannot dequeue empty queue.\n");
+        exit(EXIT_FAILURE);
     } else {
         item = q->items[q->head];
-        q->head++;
+        q->head = (q->head + 1) % q->capacity;
         q->num_items--;
-        return item; 
+        return item;
     }
 }
 
 void queue_print(queue q) {
     int i;
-    double temp;
-    for (i=0;i<q->num_items;i++) {
-        temp = q->items[(q->head + i) % q->capacity];
-        printf("%.2f\n",temp);
+    for (i=q->head;i < (q->head+q->num_items); i++) {
+        printf("%.2f\n",q->items[i % q->capacity]);
     }
 }
 
 void queue_print_info(queue q) {
     int i;
-    printf("capacity %d, num_items %d, head %d\n[",q->capacity,
-        q->num_items, q->head);
+    printf("capacity %d, num_items %d, head %d\n[",q->capacity, q->num_items, q->head);
     for (i=0; i< q->capacity; i++) {
-        printf("%s%.2f", i == 0 ? "" : ", ", q->items[i]);
+        printf("%s%.2f",i==0 ? "" : ", ", q->items[i]);
     }
     printf("]\n");
 }
@@ -74,6 +69,5 @@ int queue_size(queue q) {
 queue queue_free(queue q) {
     free(q->items);
     free(q);
-    q = NULL;
     return q;
 }
