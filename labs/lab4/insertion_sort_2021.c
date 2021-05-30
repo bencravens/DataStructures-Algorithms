b8fe40da8bf92331fd61836dee417fe5f94430a0
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-05-31 07:36:22 +1200

    Update insertion_sort_2021.c

--- labs/lab4/insertion_sort_2021.c
+++ labs/lab4/insertion_sort_2021.c
@@ -14,29 +14,13 @@ void insertion_sort(int* a, int n){
         key = a[p];
         left_p = p-1;
         
-        /*
-        printf("a[p] is %d\n",a[p]);
-        printf("a[left_p] is %d\n",a[left_p]);
-        printf("array is: \n");
-        for (i=0;i<n;i++) {
-            printf("%d\n",a[i]);
-        }
-        */
-        
         /* find position to the left of where a[p] should
         go... */
         while (a[left_p] > key && left_p >= 0) {
+            a[left_p+1]=a[left_p];
             left_p--;
         }
-        
-        /*printf("position of a[p] in array is index %d\n",left_p+1);*/
-        
-        /*now, starting with the index i=p-1, set a[i+1]=a[i]
-        (shift entry to the right) and then increment i down (i--)
-        until we are at the left most entry (i=left_p)*/
-        for(i=p-1;i>left_p;i--){
-            a[i+1] = a[i];
-        }
+        a[left_p+1]=key;
     }
 }
 
